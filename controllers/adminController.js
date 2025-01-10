af46c7ef358f4f85e0be04a94d32dddadc19053d
Jhaemis-hack <gbemilekekenny@gmail.com>
2025-01-10 19:18:08 +0100

    included proper disabling func

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -15,7 +15,7 @@ const getUsers = async (req, res) =>{
     }
 }
 
-const disabledUsers =async (req, res) => {
+const getDisabledUsers = async (req, res) =>{
     try {
         const users = await userSchema.find({role: "user", disabled: true});
 
@@ -25,7 +25,35 @@ const disabledUsers =async (req, res) => {
 
     return res.status(200).json({ users })
     } catch (error) {
-        
+        console.log(error);
+    }
+}
+
+const disableUsers =async (req, res) => {
+    try {
+        const id = req.params.id
+
+        const users = await userSchema.find({_id: id});
+
+    if(!users){
+        return res.status(404).json({message: 'No user found', data: []})
+    }
+
+    const { disabled } =req.body;
+
+    if(!disabled ){
+        return res.status(400).json({message: 'Invalid request, disabled field is required', data: null})
+    }
+    
+    const updatedDisabled = await userSchema.findByIdAndUpdate(id, {$set: {disabled: disabled}},{ new: true })    
+
+    if(disabled == "false"){
+        return res.status(200).json({message: 'User enabled successfully', updatedDisabled})
+    }
+
+    return res.status(200).json({ message: "user disabled successfullly", updatedDisabled})
+    } catch (error) {
+        console.log(error.message);
     }
 }
 
@@ -48,7 +76,7 @@ const getUser = async (req, res) => {
 
 module.exports = {
     getUsers,
-    disabledUsers,
+    disableUsers,
     getUser,
- 
+    getDisabledUsers,
 };
--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
-const { getUsers, disabledUsers, getUser } = require('../../controllers/adminController');
+const { getUsers, disableUsers, getUser, getDisabledUsers } = require('../../controllers/adminController');
 const { postBlog } = require('../../controllers/userController');
 const { jwtAuth } = require('../../Protection/Auth-Config/Auth');
 
-router.get('getall/users', jwtAuth(), getUsers)
+router.get('/users', jwtAuth(), getUsers)
+
+router.get('/disabledusers', jwtAuth(), getDisabledUsers)
 
 router.get('/user/:email', jwtAuth(), getUser) 
 
-router.post('/user/disabled', jwtAuth(), disabledUsers)
+router.post('/user/:id', jwtAuth(), disableUsers)
 
 router.post('/', jwtAuth(), postBlog)//upload.single('image')
 
